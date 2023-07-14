a7014abda877a898baeeadcaafb9fb18816e1754
Nikita Bolshakov <bnikmik@gmail.com>
2023-07-14 16:38:18 +0300

    oauth2 through github

--- src/main/java/com/example/OAuth2_TestProject/config/WebSecurityConfiguration.java
+++ src/main/java/com/example/OAuth2_TestProject/config/WebSecurityConfiguration.java
@@ -2,19 +2,31 @@ package com.example.OAuth2_TestProject.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.oauth2.client.InMemoryOAuth2AuthorizedClientService;
+import org.springframework.security.oauth2.client.OAuth2AuthorizedClientService;
 import org.springframework.security.web.SecurityFilterChain;
 
+import static org.springframework.security.config.Customizer.*;
+
 @Configuration
+@EnableWebSecurity
 public class WebSecurityConfiguration {
 
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
 
-        http.authorizeHttpRequests()
-                .mvcMatchers("/api/bye").permitAll()
-                .anyRequest().authenticated();
+        http.authorizeRequests()
+                .mvcMatchers("/bye")
+                .permitAll()
+                .anyRequest()
+                .authenticated()
+                .and()
+                .oauth2Login(withDefaults());
 
         return http.build();
     }
+
 }
--- src/main/java/com/example/OAuth2_TestProject/controller/OAuth2Controller.java
+++ src/main/java/com/example/OAuth2_TestProject/controller/OAuth2Controller.java
@@ -5,10 +5,9 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/api")
 public class OAuth2Controller {
 
-    @GetMapping("/hello")
+    @GetMapping("/")
     public String getHelloWorld() {
         return "Hello World!!!";
     }
