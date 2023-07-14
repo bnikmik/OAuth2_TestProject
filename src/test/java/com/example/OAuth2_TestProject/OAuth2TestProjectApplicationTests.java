fd089c22b2e3b871d794e2daa393b2c22c811381
Nikita Bolshakov <bnikmik@gmail.com>
2023-07-14 13:58:01 +0300

    Initial commit

---
+++ src/test/java/com/example/OAuth2_TestProject/OAuth2TestProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.OAuth2_TestProject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OAuth2TestProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
